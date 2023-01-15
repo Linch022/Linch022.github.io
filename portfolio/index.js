ac2ba6a6154e07c5750e0a09280c9b6182c44c56
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2023-01-15 18:33:25 +0100

    added skills section

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,5 @@
+// Function for homepage createletter
+
 function createLetter(elementId, text, delay, duration) {
     let element = document.getElementById(elementId);
     let lines = text.split("\n");
@@ -26,4 +28,104 @@ function createLetter(elementId, text, delay, duration) {
 
   
 
-createLetter("auto-text", "NICOLAS BERTHOLLET\nDEVELOPPEUR WEB ET MOBILE\nFULL STACK Javascript", 100, 2000);
+createLetter("auto-text", "NICOLAS BERTHOLLET\nDEVELOPPEUR WEB\nFULLSTACK Javascript", 100, 2000);
+
+// function for play section 
+
+
+function repeatEffect(){
+    let effectCount = 0;
+    let maxEffectCount = 5;
+    let intervalId = setInterval(function(){
+        const playButtonOne = document.getElementById("play-button1");
+        const playButtonTwo = document.getElementById("play-button2");
+        if(playButtonOne.style.visibility == "hidden"){
+            playButtonOne.style.visibility = "visible";
+            playButtonTwo.style.visibility = "visible";
+        } else {
+            playButtonOne.style.visibility = "hidden";
+            playButtonTwo.style.visibility = "hidden";
+            effectCount++;
+        }
+        if(effectCount >= maxEffectCount){
+            clearInterval(intervalId);
+            playButtonOne.style.visibility = "visible";
+            playButtonTwo.style.visibility = "visible";
+            effectCount = 0;
+            setTimeout(repeatEffect, 3000);
+        }
+    }, 110);
+}
+window.addEventListener("load", function() {
+    setTimeout(repeatEffect, 4500);
+});
+
+
+//  function for navbar
+const logoNavbar = document.querySelectorAll(".open-navbar");
+const navBar = document.querySelector(".navbar");
+const navList = document.querySelector(".navlist");
+
+logoNavbar.forEach(function(logo){
+        logo.addEventListener("click", function(){
+            navBar.classList.toggle("navbar-visible")
+            navList.classList.toggle("navlist-visible")
+        })
+    })
+
+
+    // function for progressbar 
+
+    let myLevel = document.querySelectorAll(".mylevel");
+
+
+function progress(idLevel, levelMax) {
+    let i = 0;
+    if (i === 0) {
+        i = 1;
+        const bar = document.getElementById(idLevel);
+        let width = 1;
+        let id = setInterval(frame, 30);
+        function frame() {
+            if (width >= levelMax) {
+                clearInterval(id)
+                i = 0;
+            } else {
+                width++;
+                bar.style.width = width + "%";
+                bar.innerHTML = width + "%";
+            }
+        }
+    }
+}
+
+function getId(selector) {
+    let elements = document.querySelectorAll(selector);
+    let ids = [];
+    elements.forEach(function(element) {
+        ids.push(element.id);
+    });
+    return ids;
+}
+
+let eventPlayed = false;
+
+window.addEventListener("scroll", () => {
+
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    console.log(scrollValue);
+    if (scrollValue > 0.60 && eventPlayed === false) {
+        let ids = getId(".mylevel");
+        let levelsValues = ["80", "80", "20", "0", "10", "75"];
+        for (let i = 0; i < ids.length; i++) {
+        progress(ids[i], levelsValues[i]);
+        } eventPlayed = true;
+    } else if (scrollValue < 0.54) {
+        eventPlayed = false;
+        for (let i = 0 ; i < myLevel.length; i++) {
+            myLevel[i].style.width = "0%";
+            myLevel[i].innerHTML = "";
+        }
+    }
+
+})
