4fb0ebe23dcfff1404b5dcbfdd565ee216bc58b7
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2023-01-16 13:51:17 +0100

    change homepage to portfolio

--- index.js
+++ index.js
@@ -1,391 +1,131 @@
-/*------------
------NAVBAR---
---------------*/
-let navBarScroll = 0;
+// Function for homepage createletter
+
+function createLetter(elementId, text, delay, duration) {
+    let element = document.getElementById(elementId);
+    let lines = text.split("\n");
+    let currentLine = 0;
+    let currentChar = 0;
+    function writeChar() {
+        if (currentLine >= lines.length) {
+            setTimeout(() => {
+                element.innerHTML = "";
+                currentLine = 0;
+                createLetter(elementId, text, delay, duration);
+            }, duration);
+            return;
+        }
+        element.innerHTML += lines[currentLine][currentChar];
+        currentChar++;
+        if (currentChar >= lines[currentLine].length) {
+            currentChar = 0;
+            currentLine++;
+            element.innerHTML += "<br>";
+        }
+        setTimeout(writeChar, delay);
+    }
+    writeChar();
+}
+
+  
+
+createLetter("auto-text", "NICOLAS BERTHOLLET\nDEVELOPPEUR WEB\nFULLSTACK Javascript", 100, 2000);
+
+// function for play section 
+
+
+function repeatEffect(){
+    let effectCount = 0;
+    let maxEffectCount = 5;
+    let intervalId = setInterval(function(){
+        const playButtonOne = document.getElementById("play-button1");
+        const playButtonTwo = document.getElementById("play-button2");
+        if(playButtonOne.style.visibility == "hidden"){
+            playButtonOne.style.visibility = "visible";
+            playButtonTwo.style.visibility = "visible";
+        } else {
+            playButtonOne.style.visibility = "hidden";
+            playButtonTwo.style.visibility = "hidden";
+            effectCount++;
+        }
+        if(effectCount >= maxEffectCount){
+            clearInterval(intervalId);
+            playButtonOne.style.visibility = "visible";
+            playButtonTwo.style.visibility = "visible";
+            effectCount = 0;
+            setTimeout(repeatEffect, 3000);
+        }
+    }, 110);
+}
+window.addEventListener("load", function() {
+    setTimeout(repeatEffect, 4500);
+});
+
+
+//  function for navbar
+const logoNavbar = document.querySelectorAll(".open-navbar");
+const navBar = document.querySelector(".navbar");
+const navList = document.querySelector(".navlist");
+
+logoNavbar.forEach(function(logo){
+        logo.addEventListener("click", function(){
+            navBar.classList.toggle("navbar-visible")
+            navList.classList.toggle("navlist-visible")
+        })
+    })
+
+
+    // function for progressbar 
+
+    let myLevel = document.querySelectorAll(".mylevel");
+
+
+function progress(idLevel, levelMax) {
+    let i = 0;
+    if (i === 0) {
+        i = 1;
+        const bar = document.getElementById(idLevel);
+        let width = 1;
+        let id = setInterval(frame, 30);
+        function frame() {
+            if (width >= levelMax) {
+                clearInterval(id)
+                i = 0;
+            } else {
+                width++;
+                bar.style.width = width + "%";
+                bar.innerHTML = width + "%";
+            }
+        }
+    }
+}
+
+function getId(selector) {
+    let elements = document.querySelectorAll(selector);
+    let ids = [];
+    elements.forEach(function(element) {
+        ids.push(element.id);
+    });
+    return ids;
+}
+
+let eventPlayed = false;
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY < navBarScroll) {
-        navbar.style.top = 0;
-    } else {
-        navbar.style.top = "-60px";
-    }
 
-    navBarScroll =  window.scrollY;
-});
-
-/*-----------------
------CAROUSEL TEAM-
--------------------*/
-const cardsContainer = document.querySelector(".card-carousel");
-const cardsController = document.querySelector(".card-carousel + .card-controller")
-
-class DraggingEvent {
-  constructor(target = undefined) {
-    this.target = target;
-  }
-  
-  event(callback) {
-    let handler;
-    
-    this.target.addEventListener("mousedown", e => {
-      e.preventDefault()
-      
-      handler = callback(e)
-      
-      window.addEventListener("mousemove", handler)
-      
-      document.addEventListener("mouseleave", clearDraggingEvent)
-      
-      window.addEventListener("mouseup", clearDraggingEvent)
-      
-      function clearDraggingEvent() {
-        window.removeEventListener("mousemove", handler)
-        window.removeEventListener("mouseup", clearDraggingEvent)
-      
-        document.removeEventListener("mouseleave", clearDraggingEvent)
-        
-        handler(null)
-      }
-    })
-    
-    this.target.addEventListener("touchstart", e => {
-      handler = callback(e)
-      
-      window.addEventListener("touchmove", handler)
-      
-      window.addEventListener("touchend", clearDraggingEvent)
-      
-      document.body.addEventListener("mouseleave", clearDraggingEvent)
-      
-      function clearDraggingEvent() {
-        window.removeEventListener("touchmove", handler)
-        window.removeEventListener("touchend", clearDraggingEvent)
-        
-        handler(null)
-      }
-    })
-  }
-  
-  // Get the distance that the user has dragged
-  getDistance(callback) {
-    function distanceInit(e1) {
-      let startingX, startingY;
-      
-      if ("touches" in e1) {
-        startingX = e1.touches[0].clientX
-        startingY = e1.touches[0].clientY
-      } else {
-        startingX = e1.clientX
-        startingY = e1.clientY
-      }
-      
-
-      return function(e2) {
-        if (e2 === null) {
-          return callback(null)
-        } else {
-          
-          if ("touches" in e2) {
-            return callback({
-              x: e2.touches[0].clientX - startingX,
-              y: e2.touches[0].clientY - startingY
-            })
-          } else {
-            return callback({
-              x: e2.clientX - startingX,
-              y: e2.clientY - startingY
-            })
-          }
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    console.log(scrollValue);
+    if (scrollValue > 0.60 && eventPlayed === false) {
+        let ids = getId(".mylevel");
+        let levelsValues = ["80", "80", "20", "0", "10", "75"];
+        for (let i = 0; i < ids.length; i++) {
+        progress(ids[i], levelsValues[i]);
+        } eventPlayed = true;
+    } else if (scrollValue < 0.54) {
+        eventPlayed = false;
+        for (let i = 0 ; i < myLevel.length; i++) {
+            myLevel[i].style.width = "0%";
+            myLevel[i].innerHTML = "";
         }
-      }
-    }
-    
-    this.event(distanceInit)
-  }
-}
-
-
-class CardCarousel extends DraggingEvent {
-  constructor(container, controller = undefined) {
-    super(container)
-    
-    // DOM elements
-    this.container = container
-    this.controllerElement = controller
-    this.cards = container.querySelectorAll(".volunteer-card")
-    
-    // Carousel data
-    this.centerIndex = (this.cards.length - 1) / 2;
-    this.cardWidth = this.cards[0].offsetWidth / this.container.offsetWidth * 100
-    this.xScale = {};
-    
-    // Resizing
-    window.addEventListener("resize", this.updateCardWidth.bind(this))
-    
-    if (this.controllerElement) {
-      this.controllerElement.addEventListener("keydown", this.controller.bind(this))      
     }
 
-    
-    // Initializers
-    this.build()
-    
-    // Bind dragging event
-    super.getDistance(this.moveCards.bind(this))
-  }
-  
-  updateCardWidth() {
-    this.cardWidth = this.cards[0].offsetWidth / this.container.offsetWidth * 100
-    
-    this.build()
-  }
-  
-  build(fix = 0) {
-    for (let i = 0; i < this.cards.length; i++) {
-      const x = i - this.centerIndex;
-      const scale = this.calcScale(x)
-      const scale2 = this.calcScale2(x)
-      const zIndex = -(Math.abs(i - this.centerIndex))
-      
-      const leftPos = this.calcPos(x, scale2)
-     
-      
-      this.xScale[x] = this.cards[i]
-      
-      this.updateCards(this.cards[i], {
-        x: x,
-        scale: scale,
-        leftPos: leftPos,
-        zIndex: zIndex
-      })
-    }
-  }
-  
-  
-  controller(e) {
-    const temp = {...this.xScale};
-      
-      if (e.keyCode === 39) {
-        // Left arrow
-        for (let x in this.xScale) {
-          const newX = (parseInt(x) - 1 < -this.centerIndex) ? this.centerIndex : parseInt(x) - 1;
-
-          temp[newX] = this.xScale[x]
-        }
-      }
-      
-      if (e.keyCode == 37) {
-        // Right arrow
-        for (let x in this.xScale) {
-          const newX = (parseInt(x) + 1 > this.centerIndex) ? -this.centerIndex : parseInt(x) + 1;
-
-          temp[newX] = this.xScale[x]
-        }
-      }
-      
-      this.xScale = temp;
-      
-      for (let x in temp) {
-        const scale = this.calcScale(x),
-              scale2 = this.calcScale2(x),
-              leftPos = this.calcPos(x, scale2),
-              zIndex = -Math.abs(x)
-
-        this.updateCards(this.xScale[x], {
-          x: x,
-          scale: scale,
-          leftPos: leftPos,
-          zIndex: zIndex
-        })
-      }
-  }
-  
-  calcPos(x, scale) {
-    let formula;
-    
-    if (x < 0) {
-      formula = (scale * 100 - this.cardWidth) / 2
-      
-      return formula
-
-    } else if (x > 0) {
-      formula = 100 - (scale * 100 + this.cardWidth) / 2
-      
-      return formula
-    } else {
-      formula = 100 - (scale * 100 + this.cardWidth) / 2
-      
-      return formula
-    }
-  }
-  
-  updateCards(card, data) {
-    if (data.x || data.x == 0) {
-      card.setAttribute("data-x", data.x)
-    }
-    
-    if (data.scale || data.scale == 0) {
-      card.style.transform = `scale(${data.scale})`
-
-      if (data.scale == 0) {
-        card.style.opacity = data.scale
-      } else {
-        card.style.opacity = 1;
-      }
-    }
-   
-    if (data.leftPos) {
-      card.style.left = `${data.leftPos}%`        
-    }
-    
-    if (data.zIndex || data.zIndex == 0) {
-      if (data.zIndex == 0) {
-        card.classList.add("highlight")
-      } else {
-        card.classList.remove("highlight")
-      }
-      
-      card.style.zIndex = data.zIndex  
-    }
-  }
-  
-  calcScale2(x) {
-    let formula;
-   
-    if (x <= 0) {
-      formula = 1 - -1 / 7 * x
-      
-      return formula
-    } else if (x > 0) {
-      formula = 1 - 1 / 7 * x
-      
-      return formula
-    }
-  }
-  
-  calcScale(x) {
-    const formula = 1 - 1 / 7 * Math.pow(x, 2)
-    
-    if (formula <= 0) {
-      return 0 
-    } else {
-      return formula      
-    }
-  }
-  
-  checkOrdering(card, x, xDist) {    
-    const original = parseInt(card.dataset.x)
-    const rounded = Math.round(xDist)
-    let newX = x
-    
-    if (x !== x + rounded) {
-      if (x + rounded > original) {
-        if (x + rounded > this.centerIndex) {
-          
-          newX = ((x + rounded - 1) - this.centerIndex) - rounded + -this.centerIndex
-        }
-      } else if (x + rounded < original) {
-        if (x + rounded < -this.centerIndex) {
-          
-          newX = ((x + rounded + 1) + this.centerIndex) - rounded + this.centerIndex
-        }
-      }
-      
-      this.xScale[newX + rounded] = card;
-    }
-    
-    const temp = -Math.abs(newX + rounded)
-    
-    this.updateCards(card, {zIndex: temp})
-
-    return newX;
-  }
-  
-  moveCards(data) {
-    let xDist;
-    
-    if (data != null) {
-      this.container.classList.remove("smooth-return")
-      xDist = data.x / 250;
-    } else {
-
-      
-      this.container.classList.add("smooth-return")
-      xDist = 0;
-
-      for (let x in this.xScale) {
-        this.updateCards(this.xScale[x], {
-          x: x,
-          zIndex: Math.abs(Math.abs(x) - this.centerIndex)
-        })
-      }
-    }
-
-    for (let i = 0; i < this.cards.length; i++) {
-      const x = this.checkOrdering(this.cards[i], parseInt(this.cards[i].dataset.x), xDist),
-            scale = this.calcScale(x + xDist),
-            scale2 = this.calcScale2(x + xDist),
-            leftPos = this.calcPos(x + xDist, scale2)
-      
-      
-      this.updateCards(this.cards[i], {
-        scale: scale,
-        leftPos: leftPos
-      })
-    }
-  }
-}
-
-const carousel = new CardCarousel(cardsContainer)
-
-/*----------------
----DISPLAY FORM---
- -----------------*/
-let formCategory = document.getElementById("form-category");
-const submitForm = document.getElementById("form");
-
-
-submitForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  formCategory = formCategory.value;
-
-  if (validateForm()) {
-
-    if (formCategory === "contact") {
-      alert("Merci pour votre message nous y répondrons rapidement");
-    } else if (formCategory === "adoption") {
-      alert("Merci pour votre demande d'adoption, nous allons prendre contact avec vous");
-    } else if (formCategory === "volunteer") {
-      alert("Nous avons pris en compte votre souhait de nous rejoindre, nous allons prendre contact avec vous");
-    }
-  } return submitForm.reset();
-});
-
-function validateForm() {
-  
-  let w = document.getElementById("name").value;
-  let x = document.getElementById("lastname").value;
-  let y = document.getElementById("email").value;
-  let z = document.getElementById("textarea").value;
-  if (w === "") {
-    alert("Merci de renseigner votre prénom");
-    return false;
-  }
-  if (x === "") {
-    alert("Merci de renseigner votre nom");
-    return false;
-  }
-  if (y === "") {
-    alert("Merci de renseigner votre Email");
-    return false;
-  }
-  if (z === "" ) {
-    alert("Merci d'écrire votre message");
-    return false;
-  } else {
-    return true;
-  }
-
-
-}
-
+})
